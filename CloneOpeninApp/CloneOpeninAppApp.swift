21a813d5103eca3f307757bccefc9600be6c30f8
Karim Amsha <karim.amsha@gmail.com>
2023-07-07 15:48:26 +0300

    First

---
+++ CloneOpeninApp/CloneOpeninAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloneOpeninAppApp.swift
+//  CloneOpeninApp
+//
+//  Created by Karim Amsha on 7.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct CloneOpeninAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
